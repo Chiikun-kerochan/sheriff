9c7b396c6f6e7b4c09cf1a51b7dd42f1105d52b1
Chiikun-kerochan <140927529+Chiikun-kerochan@users.noreply.github.com>
2025-08-07 00:43:45 +0900

    main_sheriff.py

--- main_sheriff.py
+++ main_sheriff.py
@@ -30,31 +30,6 @@ async def on_ready():
 # メッセージ受信時の処理
 @client.event
 async def on_message(message):
-    ls = []
-    ml = []
-    if message.content == aikotoba:
-        current_vc = discord.utils.get(client.voice_clients,guild=message.guild)
-        for v in client.get_all_channels():
-            if isinstance(v,discord.VoiceChannel): #vcか確認
-                ls.append(v.id) #vcチャンネルのチャンネルid
-        for i in range(0,len(ls)):
-            channel = client.get_channel(ls[i])
-            print(channel)
-            await channel.connect()
-            for k in channel.members:
-                if k is not None:
-                    ml.append(k)  ##20250806追加
-            await current_vc.disconnect()
-        for w in range(0,len(ml)):
-            guild = client.get_guild(1076105584329375765)#guild id
-            h = guild.get_member(ml[w].id)
-            if h.bot == False:
-                print(h)
-                await h.move_to(channel=None,reason="配信が始まるため")
-        await message.channel.send("任務完了")
-
-@client.event
-async def on_message(message):
     guild = client.get_guild(1076105584329375765) 
     zatsudan = client.get_channel(1076482232342020096)
     ph = guild.get_member(951411435370582016) #ふぁれんのユーザーid=1018781055215468624
@@ -71,8 +46,32 @@ async def on_message(message):
                 await pr_ch.send(f"{t}分経過")
             await channel.send(aikotoba)
 
+    ls = []
+    ml = []
+    if message.content == aikotoba:
+        for v in client.get_all_channels():
+            if isinstance(v,discord.VoiceChannel): #vcか確認
+                ls.append(v.id) #vcチャンネルのチャンネルid
+        for i in range(0,len(ls)):
+            channel = client.get_channel(ls[i])
+            print(channel)
+            await channel.connect()
+            for k in channel.members:
+                if k is not None:
+                    ml.append(k)  ##20250806追加
+            current_vc = discord.utils.get(client.voice_clients,guild=message.guild)
+            await current_vc.disconnect()
+        for w in range(0,len(ml)):
+            guild = client.get_guild(1076105584329375765)#guild id
+            h = guild.get_member(ml[w].id)
+            if h.bot == False:
+                print(h)
+                await h.move_to(channel=None,reason="配信が始まるため")
+        await message.channel.send("任務完了")
+
 keep_alive()
 client.run(TOKEN)
+
 
 
 
