24e25abfd6088b8441338fb7db5a447e9515f565
Chiikun-kerochan <140927529+Chiikun-kerochan@users.noreply.github.com>
2026-01-03 21:43:31 +0900

    main_sheriff.py

--- main_sheriff.py
+++ main_sheriff.py
@@ -433,7 +433,8 @@ channel_id = 1456890395970768951
 cont = "https://media.discordapp.net/attachments/1160135713480785921/1181510300034400339/140_20231205171931.png?ex=695979f6&is=69582876&hm=8b9f8c771b598fc1abde8c89f036e629633bacc69cb13e80a169b5b7a659095f&=&format=webp&quality=lossless"
 Use = client.get_channel(channel_id)
 
-schedule.every().day.at("21:30").do(lambda: schedule_job(f"{cont}\n 21:30です。配信の調子はいかがでしょうか。 <@1018781055215468624>", [1,2,4,5,6], channel_id))  # 
+schedule.every().day.at("21:30").do(lambda: schedule_job(f"{cont}\n 21:30です。配信の調子はいかがでしょうか。 <@1018781055215468624>", [1,2,4,5,6], channel_id))  
+schedule.every().day.at("21:45").do(lambda: schedule_job(f"{cont}\n 21:45です。そろそろ10時です。 <@1018781055215468624>", [1,2,4,5,6], channel_id))
 schedule.every().day.at("22:00").do(lambda: schedule_job(f"{cont}\n 22:00です。配信の時刻としては理想的でしょう。 <@1018781055215468624>", [1,2,4,5,6], channel_id))  # 
 
 def run_schedule():
@@ -447,6 +448,7 @@ schedule_thread.start()
 
 keep_alive()
 client.run(TOKEN)
+
 
 
 
